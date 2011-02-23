52b3c33f5a3d7833986a57133c657ec9110cfb82
Tyler Smith <tylesmit@cisco.com>
2011-02-22 20:42:24 -0500

    blueprint quantum-packaging
    Change-Id: Ica19170540b06ecddb0fbb6d340ee7a6819c1708

--- tools/source_nonplugin_environment.py
+++ tools/source_nonplugin_environment.py
@@ -1,7 +1,6 @@
-#!/bin/bash
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
-# Copyright 2011 OpenStack LLC.
+# Copyright 2011 Cisco Systems
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -15,7 +14,13 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#    @author: Tyler Smith, Cisco Systems
+import os
+import sys
 
-TOOLS=`dirname $0`
-VENV=~/.quantum-venv
-source $VENV/bin/activate && $@
+# To run from the source code, we need to add the various packages
+# to our path so we can find all of the modules correctly
+packages = ['common', 'server', 'client']
+
+for project in packages + ["plugins/sample-plugin"]:
+    sys.path.insert(0, os.path.join(os.getcwd(), project, 'lib'))
