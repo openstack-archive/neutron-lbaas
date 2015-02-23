93e10c98778000587c1faee6d8ebc7ef04831a6c
Phillip Toohill <phillip.toohill@rackspace.com>
2015-02-22 22:08:51 -0600

    Agent, device driver interface, and callback API
    LBaaS V2 object model needed an agent to redone to work with
    the new object model.  This creates the agent main script with
    a place holder agent manager used as the service.  This also
    adds the interface for device drivers.  The first device driver
    implementation will the be namespace haproxy driver redone for
    the new object model but will be added in a later review.  Lastly,
    this adds the API class that exposes the plugin callbacks.
    
    Partially-implements: blueprint lbaas-refactor-haproxy-namespace-driver-to-new-driver-interface
    Co-Authored-By: Phillip Toohill <phillip.toohill@rackspace.com>
    Co-Authored-By: Brandon Logan <brandon.logan@rackspace.com>
    Change-Id: Ie7842bd347380a7c7ed1c32c9b63ceb0adaaa58f

---
+++ neutron_lbaas/agent/agent_manager.py
@@ -0,0 +1,34 @@
+# Copyright 2013 New Dream Network, LLC (DreamHost)
+# Copyright 2015 Rackspace
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.openstack.common import periodic_task
+from oslo_config import cfg
+
+
+OPTS = [
+    cfg.MultiStrOpt(
+        'device_driver',
+        default=['neutron_lbaas.drivers.haproxy.'
+                 'namespace_driver.HaproxyNSDriver'],
+        help=_('Drivers used to manage loadbalancing devices'),
+    ),
+]
+
+
+class LbaasAgentManager(periodic_task.PeriodicTasks):
+
+    def __init__(self, conf):
+        super(LbaasAgentManager, self).__init__()
+        self.conf = conf
