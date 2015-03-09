06c4681ec68d29991a4e49478be0d15d9e7b4138
Brandon Logan <brandon.logan@rackspace.com>
2015-03-09 13:19:50 -0500

    Added tox tempest environment
    When running tox with tox -etempest, tox will install tempest
    from a specific commit that the v2 tests are known to work well
    with.  Then the tests will be run. This also puts a change in
    pretty_tox.sh to control the concurrency level because the
    tempest tests will fail when running in parallel.
    
    Change-Id: I745293322149390f0e709165f34c144f5c439720

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
 TESTRARGS=$1
+#This is for supporting tempest tests in tox as the neutron-lbaas tempest tests fail when run in parallel
+CONCURRENCY=${OS_TESTR_CONCURRENCY:-}
+if [ -n "$CONCURRENCY" ]
+then
+  CONCURRENCY="--concurrency=$CONCURRENCY"
+fi
 
 exec 3>&1
-status=$(exec 4>&1 >&3; (python setup.py testr --slowest --testr-args="--subunit $TESTRARGS"; echo $? >&4 ) | $(dirname $0)/subunit-trace.py -f) && exit $status
+status=$(exec 4>&1 >&3; (python setup.py testr --slowest --testr-args="--subunit $TESTRARGS $CONCURRENCY"; echo $? >&4 ) | $(dirname $0)/subunit-trace.py -f) && exit $status
