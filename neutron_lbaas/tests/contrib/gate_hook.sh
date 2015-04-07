f74bb4e4271ded327efb316a8533de137aee31fa
Doug Wiegley <dougw@a10networks.com>
2015-04-06 22:31:21 -0700

    Cleaner mechanism for enabling lbaasv2 in devstack-gate
    Change-Id: I90ccc55c6de61f345173c9f4f67aaf765215e12c

--- neutron_lbaas/tests/contrib/gate_hook.sh
+++ neutron_lbaas/tests/contrib/gate_hook.sh
@@ -15,27 +15,10 @@ export DEVSTACK_LOCAL_CONFIG="enable_plugin neutron-lbaas https://git.openstack.
 if [ "$testenv" != "apiv1" ]; then
   # Override enabled services, so we can turn on lbaasv2.
   # While we're at it, disable cinder and swift, since we don't need them.
-  s=""
-  #s+="c-api,c-bak,c-sch,c-vol,"
-  s+="ceilometer-acentral,ceilometer-acompute,ceilometer-alarm-evaluator"
-  s+=",ceilometer-alarm-notifier,ceilometer-anotification,ceilometer-api"
-  s+=",ceilometer-collector"
-  #s+=",cinder"
-  s+=",dstat"
-  s+=",g-api,g-reg"
-  s+=",h-api,h-api-cfn,h-api-cw,h-eng"
-  s+=",heat"
-  s+=",horizon"
-  s+=",key"
-  s+=",mysql"
-  s+=",n-api,n-cond,n-cpu,n-crt,n-obj,n-sch"
-  s+=",q-agt,q-dhcp,q-fwaas,q-l3,q-meta,q-metering,q-svc,q-vpn,quantum"
-  s+=",q-lbaasv2"
-  s+=",rabbit"
-  #s+=",s-account,s-container,s-object,s-proxy"
-  s+=",sahara"
-  s+=",tempest"
-  export OVERRIDE_ENABLED_SERVICES="$s"
+  ENABLED_SERVICES="q-lbaasv2,-q-lbaas"
+  ENABLED_SERVICES+=",-c-api,-c-bak,-c-sch,-c-vol,-cinder"
+  ENABLED_SERVICES+=",-s-account,-s-container,-s-object,-s-proxy"
+  export ENABLED_SERVICES
 fi
 
 $BASE/new/devstack-gate/devstack-vm-gate.sh
