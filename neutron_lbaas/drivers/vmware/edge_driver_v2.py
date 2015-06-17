b5d4e5c0fe02a897ad2ab0bc548f695915998831
Kobi Samoray <ksamoray@vmware.com>
2015-06-17 17:11:19 +0300

    VMWare NSXv LBaaSv2 driver
    Implement an LBaaS v2 driver for VMWare NSXv Edge appliances loadbalancer.
    Includes support for TLS termination.
    
    Depends-On: I60093c0186cce3e99fb26e1fc6bd5175cbd1a560
    Closes-Bug: #1526436
    Change-Id: Iba1ae36342032e23f43c0a1fc3e7bbc40327ce91

---
+++ neutron_lbaas/drivers/vmware/edge_driver_v2.py
@@ -0,0 +1,134 @@
+# Copyright 2015 VMware, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_log.helpers import log_method_call as call_log
+from oslo_log import log as logging
+
+from neutron_lbaas.common import cert_manager
+from neutron_lbaas.drivers import driver_base
+
+LOG = logging.getLogger(__name__)
+
+
+class EdgeDriverBaseManager(object):
+    @property
+    def nsxv_driver(self):
+        return self.driver.plugin.db._core_plugin.nsx_v
+
+
+class EdgeLoadBalancerDriverV2(driver_base.LoadBalancerBaseDriver):
+    @call_log
+    def __init__(self, plugin):
+        self.plugin = plugin
+        super(EdgeLoadBalancerDriverV2, self).__init__(plugin)
+        self.load_balancer = EdgeLoadBalancerManager(self)
+        self.listener = EdgeListenerManager(self)
+        self.pool = EdgePoolManager(self)
+        self.member = EdgeMemberManager(self)
+        self.health_monitor = EdgeHealthMonitorManager(self)
+
+
+class EdgeLoadBalancerManager(driver_base.BaseLoadBalancerManager,
+                              EdgeDriverBaseManager):
+    @call_log
+    def create(self, context, lb):
+        self.nsxv_driver.loadbalancer.create(context, lb)
+
+    @call_log
+    def update(self, context, old_lb, new_lb):
+        self.nsxv_driver.loadbalancer.update(context, old_lb, new_lb)
+
+    @call_log
+    def delete(self, context, lb):
+        self.nsxv_driver.loadbalancer.delete(context, lb)
+
+    @call_log
+    def refresh(self, context, lb):
+        return self.nsxv_driver.loadbalancer.refresh(context, lb)
+
+    @call_log
+    def stats(self, context, lb):
+        return self.nsxv_driver.loadbalancer.stats(context, lb)
+
+
+class EdgeListenerManager(driver_base.BaseListenerManager,
+                          EdgeDriverBaseManager):
+
+    def _get_default_cert(self, listener):
+        if listener.default_tls_container_id:
+            cert_backend = cert_manager.get_backend()
+            if cert_backend:
+                return cert_backend.CertManager.get_cert(
+                        listener.default_tls_container_id)
+
+    @call_log
+    def create(self, context, listener):
+        self.nsxv_driver.listener.create(
+            context, listener, certificate=self._get_default_cert(listener))
+
+    @call_log
+    def update(self, context, old_listener, new_listener):
+        self.nsxv_driver.listener.update(
+            context, old_listener, new_listener,
+            certificate=self._get_default_cert(new_listener))
+
+    @call_log
+    def delete(self, context, listener):
+        self.nsxv_driver.listener.delete(context, listener)
+
+
+class EdgePoolManager(driver_base.BasePoolManager,
+                      EdgeDriverBaseManager):
+    @call_log
+    def create(self, context, pool):
+        self.nsxv_driver.pool.create(context, pool)
+
+    @call_log
+    def update(self, context, old_pool, new_pool):
+        self.nsxv_driver.pool.update(context, old_pool, new_pool)
+
+    @call_log
+    def delete(self, context, pool):
+        self.nsxv_driver.pool.delete(context, pool)
+
+
+class EdgeMemberManager(driver_base.BaseMemberManager,
+                        EdgeDriverBaseManager):
+    @call_log
+    def create(self, context, member):
+        self.nsxv_driver.member.create(context, member)
+
+    @call_log
+    def update(self, context, old_member, new_member):
+        self.nsxv_driver.member.update(context, old_member, new_member)
+
+    @call_log
+    def delete(self, context, member):
+        self.nsxv_driver.member.delete(context, member)
+
+
+class EdgeHealthMonitorManager(driver_base.BaseHealthMonitorManager,
+                               EdgeDriverBaseManager):
+    @call_log
+    def create(self, context, hm):
+        self.nsxv_driver.healthmonitor.create(context, hm)
+
+    @call_log
+    def update(self, context, old_hm, new_hm):
+        self.nsxv_driver.healthmonitor.update(context, old_hm, new_hm)
+
+    @call_log
+    def delete(self, context, hm):
+        self.nsxv_driver.healthmonitor.delete(context, hm)
---
+++ neutron_lbaas/tests/unit/drivers/vmware/test_edge_driver_v2.py
@@ -0,0 +1,118 @@
+# Copyright 2015 VMware, Inc.
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from neutron import context as ncontext
+
+from neutron_lbaas.drivers.vmware import edge_driver_v2
+from neutron_lbaas.tests.unit.db.loadbalancer import test_db_loadbalancer
+
+DUMMY_CERT = {'id': 'fake_id'}
+
+
+class FakeModel(object):
+    def __init__(self, id):
+        self.id = id
+
+
+class ManagerTest(object):
+    def __init__(self, context, manager, model, mocked_nsxv):
+        self.context = context
+        self.manager = manager
+        self.model = model
+        self.mocked_nsxv = mocked_nsxv
+
+        self.create(model)
+        self.update(model, model)
+        self.delete(model)
+
+    def create(self, model):
+        self.manager.create(self.context, model)
+        if model.id == 'listener':
+            model.default_tls_container_id = 'fake_id'
+            self.mocked_nsxv.create.assert_called_with(
+                self.context, model, certificate=DUMMY_CERT)
+        else:
+            self.mocked_nsxv.create.assert_called_with(self.context, model)
+
+    def update(self, old_model, model):
+        self.manager.update(self.context, old_model, model)
+        if model.id == 'listener':
+            self.mocked_nsxv.update.assert_called_with(
+                self.context, old_model, model, certificate=DUMMY_CERT)
+        else:
+            self.mocked_nsxv.update.assert_called_with(self.context,
+                                                       old_model, model)
+
+    def delete(self, model):
+        self.manager.delete(self.context, model)
+        self.mocked_nsxv.delete.assert_called_with(self.context, model)
+
+    def refresh(self):
+        self.manager.refresh(self.context, self.model)
+        self.mocked_nsxv.refresh.assert_called_with(self.context, self.model)
+
+    def stats(self):
+        self.manager.stats(self.context, self.model)
+        self.mocked_nsxv.stats.assert_called_with(self.context, self.model)
+
+
+class TestVMWareEdgeLoadBalancerDriverV2(
+        test_db_loadbalancer.LoadBalancerPluginDbTestCase):
+
+    def setUp(self):
+        super(TestVMWareEdgeLoadBalancerDriverV2, self).setUp()
+        self.context = ncontext.get_admin_context()
+        self.driver = edge_driver_v2.EdgeLoadBalancerDriverV2(self.plugin)
+
+    def _patch_manager(self, mgr):
+        mgr.driver = mock.Mock()
+        mgr.driver.plugin.db = mock.Mock()
+        mgr.driver.plugin.db._core_plugin = mock.Mock()
+        mgr.driver.plugin.db._core_plugin.nsx_v = mock.Mock()
+        return mgr.driver.plugin.db._core_plugin.nsx_v
+
+    def test_load_balancer_ops(self):
+        mock_nsxv_driver = self._patch_manager(self.driver.load_balancer)
+        m = ManagerTest(self, self.driver.load_balancer,
+                        FakeModel("loadbalancer"),
+                        mock_nsxv_driver.loadbalancer)
+        m.refresh()
+        m.stats()
+
+    def test_listener_ops(self):
+        mock_nsxv_driver = self._patch_manager(self.driver.listener)
+        self.driver.listener._get_default_cert = mock.Mock()
+        self.driver.listener._get_default_cert.return_value = DUMMY_CERT
+        listener = FakeModel("listener")
+        listener.default_tls_container_id = None
+        ManagerTest(self, self.driver.listener, listener,
+                    mock_nsxv_driver.listener)
+
+    def test_pool_ops(self):
+        mock_nsxv_driver = self._patch_manager(self.driver.pool)
+        ManagerTest(self, self.driver.pool, FakeModel("pool"),
+                    mock_nsxv_driver.pool)
+
+    def test_member_ops(self):
+        mock_nsxv_driver = self._patch_manager(self.driver.member)
+        ManagerTest(self, self.driver.member, FakeModel("member"),
+                    mock_nsxv_driver.member)
+
+    def test_health_monitor_ops(self):
+        mock_nsxv_driver = self._patch_manager(self.driver.health_monitor)
+        ManagerTest(self, self.driver.health_monitor, FakeModel("hm"),
+                    mock_nsxv_driver.healthmonitor)
