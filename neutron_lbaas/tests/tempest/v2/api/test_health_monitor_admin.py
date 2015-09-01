1a653f41151231c8179bbe1a80deda239014cc6d
venkata anil <anil.venkata@enovance.com>
2015-09-01 11:53:25 +0000

    lbaasv2: validate tenant-id is non-empty in api
    Lbaasv2 is not validating if the tenant-id passed in api is non-empty.
    Because of this, drivers behavior is not same for the api for empty tenant-id.
    To avoid this, we will validate tenant-id is non-empty at plugin.
    
    Closes-bug: #1451621
    Change-Id: I9d74a8bd9881dcff0e8d880624da9a48463aa31a

--- neutron_lbaas/tests/tempest/v2/api/test_health_monitor_admin.py
+++ neutron_lbaas/tests/tempest/v2/api/test_health_monitor_admin.py
@@ -14,6 +14,7 @@
 
 from oslo_log import log as logging
 from oslo_utils import uuidutils
+from tempest_lib import exceptions as ex
 
 from neutron_lbaas.tests.tempest.lib.common.utils import data_utils
 from neutron_lbaas.tests.tempest.lib import config
@@ -74,18 +75,17 @@ class TestHealthMonitors(base.BaseAdminTestCase):
         hm_tenant_id = hm.get('tenant_id')
         self.assertEqual(admin_tenant_id, hm_tenant_id)
 
-    @test.attr(type='smoke')
+    @test.attr(type='negative')
     def test_create_health_monitor_empty_tenant_id_field(self):
         """
-        Test with admin user create health monitor with an empty tenant id
-        field.
+        Test with admin user creating health monitor with an empty tenant id
+        field should fail.
         """
-        hm = self._create_health_monitor(type='HTTP', delay=3, max_retries=10,
-                                         timeout=5,
-                                         pool_id=self.pool.get('id'),
-                                         tenant_id="")
-
-        self.assertEqual(hm.get('tenant_id'), "")
+        self.assertRaises(ex.BadRequest, self._create_health_monitor,
+                          type='HTTP', delay=3, max_retries=10,
+                          timeout=5,
+                          pool_id=self.pool.get('id'),
+                          tenant_id="")
 
     @test.attr(type='smoke')
     def test_create_health_monitor_for_another_tenant_id_field(self):
