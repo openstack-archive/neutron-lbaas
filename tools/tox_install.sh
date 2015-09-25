62bdafdb688d63109bf1fe0f815ecd00d26cd4d4
Ihar Hrachyshka <ihrachys@redhat.com>
2015-09-25 13:02:20 +0200

    Use stable/liberty branch for neutron dep
    Change-Id: Ie9469a3b936eb1849f10a96a99d9c09fe39a8fc5

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -34,7 +34,7 @@ elif [ -x "$ZUUL_CLONER" ]; then
     cd "$cwd"
 else
     echo "PIP HARDCODE" > /tmp/tox_install.txt
-    pip install -U -egit+https://git.openstack.org/openstack/neutron#egg=neutron
+    pip install -U -egit+https://git.openstack.org/openstack/neutron@stable/liberty#egg=neutron
 fi
 
 pip install -U $*
