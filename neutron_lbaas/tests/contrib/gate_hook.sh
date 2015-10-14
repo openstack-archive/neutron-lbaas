1094298077a9307e563b3713e4bdbf5007bb73ba
Ihar Hrachyshka <ihrachys@redhat.com>
2015-10-14 10:32:45 +0200

    Use noop octavia driver for minimal job too
    Seems like without the driver being set, octavia attempts to run the
    load balancer in an amphora, and it fails for some reason to transit the
    object into ACTIVE state.
    Also remove the amphora boot related gate settings.
    
    Change-Id: I1eb239ff18fb7ad77d0839f725029a703bd14982
    Closes-Bug: #1505932
    (cherry picked from commit 6de135a973bd8225dcabd799ae4a06f32ee0c744)

--- neutron_lbaas/tests/contrib/gate_hook.sh
+++ neutron_lbaas/tests/contrib/gate_hook.sh
@@ -10,7 +10,7 @@ testenv=${2:-"apiv2"}
 if [ "$1" = "lbaasv1" ]; then
     testenv="apiv1"
 elif [ "$1" = "lbaasv2" ]; then
-    if [ "$2" = "healthmonitor" ] || [ "$2" = "listener" ] || [ "$2" = "loadbalancer" ] || [ "$2" = "member" ] || [ "$2" = "pool" ]; then
+    if [ "$2" = "healthmonitor" ] || [ "$2" = "listener" ] || [ "$2" = "loadbalancer" ] || [ "$2" = "member" ] || [ "$2" = "minimal" ] || [ "$2" = "pool" ]; then
         testenv="apiv2"
     elif [ "$2" = "scenario" ]; then
           testenv="scenario"
@@ -21,7 +21,6 @@ export DEVSTACK_LOCAL_CONFIG+="
 enable_plugin neutron-lbaas https://git.openstack.org/openstack/neutron-lbaas
 enable_plugin barbican https://git.openstack.org/openstack/barbican
 enable_plugin octavia https://git.openstack.org/openstack/octavia
-IMAGE_URLS+=\",http://g8de985af226d635b09bbb525b05dc4af.cdn.hpcloudsvc.com/amphora/amphora-x64-haproxy.qcow2\"
 "
 
 if [ "$testenv" != "apiv1" ]; then
@@ -32,8 +31,8 @@ if [ "$testenv" != "apiv1" ]; then
   ENABLED_SERVICES+=",-s-account,-s-container,-s-object,-s-proxy"
   ENABLED_SERVICES+=",octavia,o-cw,o-hk,o-hm,o-api"
   export ENABLED_SERVICES
-  VOLUME_BACKING_FILE_SIZE=24G
-  export VOLUME_BACKING_FILE_SIZE
+  DISABLE_AMP_IMAGE_BUILD=True
+  export DISABLE_AMP_IMAGE_BUILD
 fi
 
 if [ "$testenv" = "apiv2" ]; then
