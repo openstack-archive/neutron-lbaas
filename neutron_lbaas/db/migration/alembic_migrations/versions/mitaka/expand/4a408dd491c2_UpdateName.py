cb3ae497c0a6349dfea0a41788b962a4cd3ef3eb
Reedip Banerjee <reedip.banerjee@nectechnologies.in>
2015-11-13 12:32:27 +0530

    Support for Name field in Members and HMs
    This patch adds support to enable naming LBaasV2 Members and Health
    Monitors(HMs).
    
    DocImpact
    
    Closes-Bug: #1515506
    Change-Id: Ieb66386fac3a5a4dace0112838fe9afde212f055

---
+++ neutron_lbaas/db/migration/alembic_migrations/versions/mitaka/expand/4a408dd491c2_UpdateName.py
@@ -0,0 +1,35 @@
+# Copyright 2015 NEC Corporation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Addition of Name column to lbaas_members and lbaas_healthmonitors table
+
+Revision ID: 4a408dd491c2
+Revises: 3345facd0452
+Create Date: 2015-11-16 11:47:43.061649
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4a408dd491c2'
+down_revision = '3345facd0452'
+
+from alembic import op
+import sqlalchemy as sa
+
+LB_TAB_NAME = ['lbaas_members', 'lbaas_healthmonitors']
+
+
+def upgrade():
+    for table in LB_TAB_NAME:
+        op.add_column(table, sa.Column('name', sa.String(255), nullable=True))
