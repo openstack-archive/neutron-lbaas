fd55125641c9458369842f72e1fc65060468ef5f
Michael Johnson <johnsomor@gmail.com>
2018-06-29 09:35:46 -0700

    Re-enable KVM
    A recent kernel update in the nodepool images has resolved the issue
    we were seeing with using KVM on some hosting providers.
    This patch removes the exclusion for those hosting providers and
    allows the neutron-lbaas gates to use KVM if it is available on the host.
    
    Change-Id: I660f2cc915819a6181bfc25145bc567bf981bc53

--- neutron_lbaas/tests/contrib/gate_hook.sh
+++ neutron_lbaas/tests/contrib/gate_hook.sh
@@ -31,7 +31,7 @@ enable_plugin barbican https://git.openstack.org/openstack/barbican
 # resolve the KVM failures as logged here:
 # https://bugzilla.kernel.org/show_bug.cgi?id=192521
 # However, this may be resolved at OVH before the kernel bug is resolved.
-if $(egrep --quiet '(vmx|svm)' /proc/cpuinfo) && [[ ( ! $(hostname) =~ "ovh" || ! $(hostname) =~ "limestone" ) ]]; then
+if egrep --quiet '(vmx|svm)' /proc/cpuinfo; then
     export DEVSTACK_GATE_LIBVIRT_TYPE=kvm
 fi
 
